87ead804cbf88423db6beedcbf2786031567f713
Frank Duncan <frank@kank.net>
2022-04-04 05:58:33 -0500

    Moved to https://code.librehq.com/ots/EmulateGroup

--- sql/currentlyemulating.sql
+++
@@ -1,6 +0,0 @@
--- MySQL/SQLite schema for the Teamteamcomments extension
-
-CREATE TABLE /*_*/EmulateGroupCurrentlyEmulating (
-  user_id int(11) NOT NULL UNIQUE,
-  group_name varchar(255) NOT NULL
-) /*$wgDBTableOptions*/;
